e92b6915130f604891936cde05d2a96a9b766848
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-02-02 07:36:20 +0200

    Updated documentation

--- sidebars.js
+++ sidebars.js
@@ -1,39 +1,50 @@
+
 module.exports = {
   someSidebar: {
-    Introduction: ["framework-introduction", "framework-roadmap"],
-    Foundation: [
-      "package-core",
-      {
-        type: "category",
-        label: "Database",
-        items: ["package-mongo", "package-nova"],
-      },
-      "package-logger",
-      {
-        type: "category",
-        label: "Security",
-        items: [
-          "package-security",
-          "package-security-mongo",
-          "package-password-bundle",
-        ],
-      },
-      {
-        type: "category",
-        label: "GraphQL",
-        items: ["package-graphql", "package-apollo", "package-apollo-security"],
-      },
-      "package-emails",
-      "package-terminal",
-      "package-validator-bundle",
+    "Meta": [
+        "framework-introduction",
+        "framework-roadmap"
+    ],
+    "Foundation": [
+        "package-core",
+        {
+            "type": "category",
+            "label": "Database",
+            "items": [
+                "package-mongo",
+                "package-nova"
+            ]
+        },
+        "package-logger",
+        {
+            "type": "category",
+            "label": "Security",
+            "items": [
+                "package-security",
+                "package-security-mongo",
+                "package-password-bundle"
+            ]
+        },
+        {
+            "type": "category",
+            "label": "GraphQL",
+            "items": [
+                "package-graphql",
+                "package-apollo",
+                "package-apollo-security"
+            ]
+        },
+        "package-emails",
+        "package-terminal",
+        "package-validator-bundle"
     ],
     "X-Framework": [
-      "x-framework-introduction",
-      "x-framework-conventions",
-      "package-x-bundle",
-      "package-x-ui",
-      "package-x-generator-bundle",
-      "package-x-password-bundle",
-    ],
-  },
-};
+        "x-framework-introduction",
+        "package-x-bundle",
+        "package-x-ui",
+        "package-x-generator-bundle",
+        "package-x-password-bundle",
+        "x-framework-conventions"
+    ]
+}
+}
